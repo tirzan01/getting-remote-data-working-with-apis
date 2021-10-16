db5da479908f31499b2fa656f5b590a3b797d91a
thomas <thomasmonta98@gmail.com>
2021-10-16 17:32:25 +1100

    viewed

--- lib/nyc_api.rb
+++ lib/nyc_api.rb
@@ -12,7 +12,15 @@ class GetPrograms
     response.body
   end
 
+  def program_school
+    # we use the JSON library to parse the API response into nicely formatted JSON
+      programs = JSON.parse(self.get_programs)
+      programs.collect do |program|
+        program["agency"]  
+      end
+    end
+
 end
 
-programs = GetPrograms.new.get_programs
-puts programs
+programs = GetPrograms.new.program_school
+puts programs.uniq
